97a6b39c35bba5418af7446012e755176a29f006
Jordan Van Ness <jordanwvn@gmail.com>
2017-11-13 14:25:47 -0800

    basic beginning and starting comment

---
+++ app.js
@@ -0,0 +1,6 @@
+/* RuneQuest Character Sheet **************************************************
+* This web app is designed to act as a character sheet for the RPG RuneQuest. *
+* The user creates a character by going through the options provided by the   *
+* base rules of the game. Other attributes are then calculated from those. In *
+* the end, the character will be able to engage in combat and level up.       *
+****************************************************************/ 'use strict';
